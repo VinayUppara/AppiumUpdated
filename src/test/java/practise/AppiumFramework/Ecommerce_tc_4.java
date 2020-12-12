23e488434164ebd25db522524730f9101fc30fa9
Vinay_SR71 <kumarvinayu@gmail.com>
2020-12-12 12:15:39 +0530

    Changes made to ecommerce test

--- src/test/java/practise/AppiumFramework/Ecommerce_tc_4.java
+++ src/test/java/practise/AppiumFramework/Ecommerce_tc_4.java
@@ -36,14 +36,16 @@ public class Ecommerce_tc_4 extends base{
 		u.scrollToText("Argentina");
 		//driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));");
 		//   driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textMatches(\"" + containedText + "\").instance(0))"));     
-		driver.findElement(By.xpath("//*[@text='Argentina']")).click();
+		driver.findElement(By.xpath("//*[@text='Australia']")).click();
 		driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
 
-		driver.findElements(By.xpath("//*[@text='ADD TO CART']")).get(0).click();
+		//driver.findElements(By.xpath("//*[@text='ADD TO CART']")).get(0).click();
 		driver.findElements(By.xpath("//*[@text='ADD TO CART']")).get(0).click();
 		driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
 		Thread.sleep(4000);
-		int count=driver.findElements(By.id("com.androidsample.generalstore:id/productPrice")).size();
+		driver.findElementByClassName("android.widget.CheckBox").click();
+		driver.findElement(By.id("com.androidsample.generalstore:id/btnProceed")).click();
+		/*int count=driver.findElements(By.id("com.androidsample.generalstore:id/productPrice")).size();
 		double sum=0;
 		CheckoutPage checkOutPage=new CheckoutPage(driver);
 
@@ -60,7 +62,7 @@ public class Ecommerce_tc_4 extends base{
 		total= total.substring(1);
 		double totalValue=Double.parseDouble(total);
 		System.out.println(totalValue+"Total value of products");
-		Assert.assertEquals(sum, totalValue);  
+		Assert.assertEquals(sum, totalValue);  */
 		service.stop();
 
 
